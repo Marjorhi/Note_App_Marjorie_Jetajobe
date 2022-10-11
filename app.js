44afd1cfbd61643d4aeafd55e1aa7a0ebe2548ac
Marjorie_Jetajobe <marjoriejetajobe0524@gmail.com>
2022-10-12 02:14:20 +0800

    Getting the user input to app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const add = require('./add')
 const write = require('./write')
-const cmd = process.argv[2]
-write(cmd);
 const read = require ('./read')
-console.log(read())
+
+// get user input
+const data = process.argv
