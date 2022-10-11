bd6e58eeed7f8289de9d2f0134522b863c1dccb9
Marjorie_Jetajobe <marjoriejetajobe0524@gmail.com>
2022-10-12 02:16:17 +0800

    Building the object to app.js

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const read = require ('./read')
 
 // get user input
 const data = process.argv
+
+var note = {}
+
+if(data[2] == 'add') {
+    // Build Object
+    note = { 
+        id: data[3],
+        title: data[4],
+        body: data[5]
+    }
+}
